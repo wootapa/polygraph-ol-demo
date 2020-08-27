e1eff38e37d3f050bc4a9761da2a67ffd0518da8
Andreas Petersson <apesv03@gmail.com>
2020-08-27 21:23:58 +0200

    Disable doubletap/pinch zoom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'waoe-demo';
+
+  constructor() {
+    document.addEventListener('gesturestart', e => e.preventDefault());
+  }
 }
