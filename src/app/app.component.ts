825fac580ff7e278c86c8407334345d0213c80df
Andreas Petersson <apesv03@gmail.com>
2021-12-29 16:44:40 +0100

    polygraph update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'waoe-demo';
+  title = 'polygraph-demo';
 
   constructor() {
     document.addEventListener('gesturestart', e => e.preventDefault());
